f1d405eab5fa59f08c33dbb49946a400765ff6f3
an920107 <an920107@gmail.com>
2024-04-10 10:26:23 +0800

    add: add YT icon to footer.

--- frontend/src/components/layout/footer.tsx
+++ frontend/src/components/layout/footer.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 
@@ -10,7 +10,7 @@ export default function Footer({ }: Props) {
     <footer className="border-t border-t-slate-200">
       <div className="container flex flex-wrap justify-center items-center py-12 gap-x-6">
         <div className="flex gap-x-4">
-
+        <a href="https://www.youtube.com/@squidspirit16"><FontAwesomeIcon icon={faYoutube} className="h-4 w-4"></FontAwesomeIcon></a>
         <a href="mailto:an920107@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="h-4 w-4"></FontAwesomeIcon></a>
         <a href="https://github.com/an920107/blog"><FontAwesomeIcon icon={faGithub} className="h-4 w-4"></FontAwesomeIcon></a>
         </div>
