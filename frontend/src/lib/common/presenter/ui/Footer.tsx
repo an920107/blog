75bb3fbeba87cb711b4c446f247697dc6d477a65
SquidSpirit <squid@squidspirit.com>
2025-01-18 04:21:36 +0800

    BLOG-13 Add link to footer icons (#18)
    - squid 3853160bf3 BLOG-13 fix: add link to footer icons
    
    Reviewed-on: https://git.squidspirit.com/squid/blog/pulls/18
    Co-authored-by: SquidSpirit <squid@squidspirit.com>
    Co-committed-by: SquidSpirit <squid@squidspirit.com>

--- frontend/src/lib/common/presenter/ui/Footer.tsx
+++ frontend/src/lib/common/presenter/ui/Footer.tsx
@@ -1,15 +1,34 @@
-import { faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faGitAlt, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <div className="border-t border-gray-300">
       <div className="mx-auto flex max-w-screen-xl flex-col items-center justify-center gap-4 px-4 py-12 md:flex-row md:px-6">
         <div className="flex flex-row items-center justify-center gap-x-4">
-          <FontAwesomeIcon className="size-4" icon={faYoutube} />
-          <FontAwesomeIcon className="size-4" icon={faEnvelope} />
-          <FontAwesomeIcon className="size-4" icon={faGithub} />
+          <Link href="https://www.youtube.com/@squidspirit16" target="_blank">
+            <FontAwesomeIcon
+              className="size-4"
+              icon={faYoutube}
+              title="YouTube Channel"
+            />
+          </Link>
+          <Link href="mailto:squid@squidspirit.com">
+            <FontAwesomeIcon
+              className="size-4"
+              icon={faEnvelope}
+              title="Email"
+            />
+          </Link>
+          <Link href="https://git.squidspirit.com/squid/blog" target="_blank">
+            <FontAwesomeIcon
+              className="size-4"
+              icon={faGitAlt}
+              title="Git Repository"
+            />
+          </Link>
         </div>
         <Devider className="max-md:hidden" />
         <span className="text-sm">Copyright © 2025 SquidSpirit</span>
