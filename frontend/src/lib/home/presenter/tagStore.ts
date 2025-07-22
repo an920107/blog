eb4b2f4d6a544552bd73b727a2714eaa0cea434a
SquidSpirit <squid@squidspirit.com>
2025-07-23 05:19:36 +0800

    BLOG-58 refactor: remove nextjs code

--- frontend/src/lib/home/presenter/tagStore.ts
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import tagReducer, { TagAction, TagState } from "@/lib/home/presenter/tagReducer";
-
-export default configureStore<TagState, TagAction>({
-  reducer: tagReducer,
-});
