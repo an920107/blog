9e0ad8f2c64436ce147968b4e715ec9f57fba581
SquidSpirit <squid@squidspirit.com>
2026-04-05 04:52:48 +0800

    BLOG-274 Add title to terms page (#279)
    ### Description
    
    As the title.
    
    ### Package Changes
    
    _No response_
    
    ### Screenshots
    
    _No response_
    
    ### Reference
    
    Resolves #274.
    
    ### Checklist
    
    - [x] A milestone is set
    - [x] The related issuse has been linked to this branch
    
    Reviewed-on: https://git.squidspirit.com/squid/blog/pulls/279
    Co-authored-by: SquidSpirit <squid@squidspirit.com>
    Co-committed-by: SquidSpirit <squid@squidspirit.com>

--- frontend/src/routes/terms/[name]/+page.ts
+++ frontend/src/routes/terms/[name]/+page.ts
@@ -1,11 +1,20 @@
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
+import { Strings } from '$lib/strings';
 
-const TERMS_DOCUMENTS = ['privacy-policy', 'ai-usage-policy'];
+interface TermsDocument {
+	pathname: string;
+	title: string;
+}
+
+const TERMS_DOCUMENTS: TermsDocument[] = [
+	{ pathname: 'privacy-policy', title: Strings.PRIVACY_POLICY },
+	{ pathname: 'ai-usage-policy', title: Strings.AI_USAGE_POLICY },
+];
 
 export const load: PageLoad = async ({ fetch, params }) => {
 	const { name } = params;
-	if (!TERMS_DOCUMENTS.includes(name)) {
+	if (!TERMS_DOCUMENTS.map((doc) => doc.pathname).includes(name)) {
 		error(404, { message: `Terms document "${params.name}" not found` });
 	}
 
@@ -17,6 +26,7 @@ export const load: PageLoad = async ({ fetch, params }) => {
 	}
 
 	return {
+		title: TERMS_DOCUMENTS.find((doc) => doc.pathname === name)?.title,
 		content: await response.text(),
 	};
 };
